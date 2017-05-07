fc90e4ec94d0d3c12f24ed5f1dbb9f698622ec66
甜虾 <daizhen.dz@alibaba-inc.com>
2017-05-08 00:26:14 +0800

    m

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,55 @@
 <template>
     <div class="form">
-        <input class="validate-item" type="text" data-errorTip="输入错误" v-model="msg">
-        <button @click="onClick">点击</button>
+        <div class="form-item">
+            <span class="from-key">姓名：</span>
+            <input class="validate-item validate-item-name" type="text" :value="name">
+            <span class="validate-tip"></span>
+        </div>
+        <div class="form-item">
+            <span class="from-key">电话：</span>
+            <input class="validate-item validate-item-tel" type="text" :value="tel">
+            <span class="validate-tip"></span>
+        </div>
+        <div class="form-item">
+            <span class="from-key">国籍：</span>
+            <select class="validate-item-nation" :value="nation">
+                <option value=""></option>
+                <option value="1">中国</option>
+                <option value="2">美国</option>
+                <option value="3">英国</option>
+                <option value="4">法国</option>
+            </select>
+            <span class="validate-tip"></span>
+        </div>
+        <div class="form-item">
+            <span class="from-key">性别：</span>
+            <label><input class="validate-item validate-item-sex" type="radio" name="sex" value="0" v-model="sex"><span>男</span></label>
+            <label><input class="validate-item validate-item-sex" type="radio" name="sex" value="1" v-model="sex"><span>女</span></label>
+            <span class="validate-tip"></span>
+        </div>
+        <div class="form-item">
+            <span class="from-key">爱好：</span>
+            <label>
+                <input class="validate-item validate-item-hobby" type="checkbox" value="reading" v-model="hobbies">
+                <span>看书</span>
+            </label>
+            <label>
+                <input class="validate-item validate-item-hobby" type="checkbox" value="run" v-model="hobbies">
+                <span>跑步</span>
+            </label>
+            <label>
+                <input class="validate-item validate-item-hobby" type="checkbox" value="swim" v-model="hobbies">
+                <span>游泳</span>
+            </label>
+            <label>
+                <input class="validate-item validate-item-hobby" type="checkbox" value="sleep" v-model="hobbies">
+                <span>睡觉</span>
+            </label>
+            <span class="validate-tip"></span>
+        </div>
+        <div class="form-item">
+            <button @click="onClick">提交</button>
+        </div>
     </div>
 </template>
 
@@ -10,17 +58,84 @@ export default {
     name: 'hello',
     data () {
         return {
-            msg: 'Welcome to Your Vue.js App'
+            name: '',
+            tel: '',
+            nation: '',
+            sex: '0',
+            hobbies: [],
+            validateFormConfig: {
+                items: [
+                    {
+                        name: 'name',
+                        type: 'text',
+                        validators: [{
+                            type: 'required',
+                            options: {
+                                error: '姓名必填'
+                            } 
+                        }, {
+                            type: 'length',
+                            options: {
+                                min: 4,
+                                max: 10,
+                                error: '长度在4~10个字符之间'
+                            } 
+                        }]
+                    }, {
+                        name: 'tel',
+                        type: 'text',
+                        validators: [{
+                            type: 'required',
+                            options: {
+                                error: '手机号码必填'
+                            }
+                        }, {
+                            type: 'regexp',
+                            options: {
+                                regexp: /^1[34578]\d{9}$/,
+                                error: '手机号码格式不正确'
+                            }
+                        }]
+                    }, {
+                        name: 'nation',
+                        type: 'select',
+                        validators: [{
+                            type: 'required',
+                            options: {
+                                error: '国籍必选'
+                            }
+                        }]
+                    }, {
+                        name: 'sex',
+                        type: 'radio',
+                        validators: [{
+                            type: 'required',
+                            options: {
+                                error: '性别必选'
+                            }
+                        }]
+                    }, {
+                        name: 'hobby',
+                        type: 'checkbox',
+                        validators: [{
+                            type: 'required',
+                            options: {
+                                error: '爱好必选'
+                            }
+                        }]
+                    }
+                ]
+            }
         }
     },
 
     mounted () {
-        this.$validateForm('.form')
+        this.$validateForm('.form', this.validateFormConfig);
     },
 
     methods: {
         onClick () {
-            this.msg = 'test';
+            
         }
     }
 
--- src/components/ValidatePlugin.js
+++ src/components/ValidatePlugin.js
@@ -1,24 +1,133 @@
 import $ from 'jquery';
-
-export default {
-	install(Vue) {
-		Vue.prototype.$validateForm = (selector) => {
-            this.$form = $(selector);
-            // this.$form.find('.validate')
-            console.log(this.$form.find('.validate-item').length);
+//*TODO checkbox
+/* ajax
+** 提交
+** 显示后台错误信息
+** 。。。。
+*/
+let validatePlugin = {};
+let EVENT_CONFIG = {
+    text: ['input', 'blur'],
+    select: ['change'],
+    radio: ['change'],
+    checkbox: ['change']
+};
+validatePlugin.install = function (Vue) {
+    Vue.prototype.$validateForm = function (formSelector, options) {
+        let $form = $(formSelector),
+            items = options.items;
+        for(let i = 0, len = items.length; i < len; i ++) {
+            bindEvent(items[i]);
         }
 
-        Vue.directive("validate", {
-            bind () {
-                console.log("bind"); 
-            },
+        function bindEvent (item) {
+            let type = EVENT_CONFIG[item.type];
+            for(let i = 0, len = type.length; i < len; i ++) {
+                let $item = $form.find('.validate-item-' + item.name);
+                $item.on(type[i], function() {
+                    validateItem($(this), item.validators);
+                })
+            }
+        }
 
-            update (el, binding, vnode) {
-                console.log(el);
-                console.log(binding);
-                console.log(binding.value, binding.oldValue);
-                console.log(vnode);
-            },
-        })
-	}
+        function validateItem ($item, validators) {
+            for(let i = 0, len = validators.length; i < len; i ++) {
+                let validator = validators[i],
+                    type = validator.type;
+                if(type === 'required') {
+                    if(!validateRrquired($item, validator)) {
+                        return;
+                    }
+                } else if(type === 'length') {
+                    if(!validateLength($item, validator)) {
+                        return;
+                    }
+                } else if(type === 'regexp') {
+                    if(!validateRegExp($item, validator)) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        function validateRrquired ($item, validator) {
+            let inputType = $item[0].type;
+            if(inputType === 'checkbox' && $item[0].checked) {
+                removeValidateTips($item);
+                return true;
+            }
+            let value = $.trim($item.val());
+            if(value === '') {
+                console.log('required error');
+                showValidateTips($item, validator);
+                return false;
+            } else {
+                removeValidateTips($item);
+                return true;
+            }
+        }
+
+        function validateLength ($item, validator) {
+            console.log(this);
+            let value = $.trim($item.val()),
+                length = value.length,
+                options = validator.options;
+            if(length > options.max || length < options.min) {
+                console.log('length error');
+                showValidateTips($item, validator);
+                return false;
+            } else {
+                removeValidateTips($item);
+                return true;
+            }
+        }
+
+        function validateRegExp ($item, validator) {
+            let value = $.trim($item.val()),
+                options = validator.options;
+            if(!options.regexp.test(value)) {
+                console.log('regexp error');
+                showValidateTips($item, validator);
+                return false;
+            } else {
+                removeValidateTips($item);
+                return true;
+            }
+        }
+
+        function showValidateTips ($item, validator) {
+            let $tip = $item.parent().find('.validate-tip'),
+                tip = '❌';
+            switch(validator.type) {
+                case 'required':
+                case 'length':
+                case 'regexp':
+                    tip += validator.options.error;
+                    break;
+                default: 
+                    console.log(33);
+            }
+
+            $tip.html(tip).show();
+        }
+
+        function removeValidateTips ($item) {
+            let $tip = $item.parents('.form-item').find('.validate-tip');
+            $tip.html('✅').show();
+        }
+    }
 }
+
+//支持 CommonJS
+if (typeof exports == "object") {
+    module.exports = validatePlugin
+// 支持 AMD
+} else if (typeof define == "function" && define.amd) {
+    define([], function(){ return validatePlugin })
+// Vue 是全局变量时，自动调用 Vue.use()
+} else if (typeof window !== 'undefined' && window.Vue) {
+    window.VueTouch = validatePlugin
+    Vue.use(validatePlugin)
+}
+
+export default validatePlugin
